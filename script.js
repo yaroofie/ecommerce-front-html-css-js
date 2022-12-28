3e4c2acf54c69cb179d4ad663fe4d84f29c879b3
yaroofie <yaroofie@gmail.com>
2022-12-28 14:48:37 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// copy menu for mobile
+function copyMenu() {
+  // copy inside .dpt-cat to .departments
+  var dptCategory = document.querySelector(".dpt-cat");
+  var dptPlace = document.querySelector(".departments");
+  dptPlace.innerHTML = dptCategory.innerHTML;
+
+  // copy inside nav to nav
+  var mainNav = document.querySelector(".header-nav nav");
+  var navPlace = document.querySelector(".off-canvas nav");
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  // copy .header-top .wrapper to .thetop-nav
+  var topNav = document.querySelector(".header-top .wrapper");
+  var topPlace = document.querySelector(".off-canvas .thetop-nav");
+  topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+// show mobile menu
+const menuButton = document.querySelector(".trigger"),
+  closeButton = document.querySelector(".t-close"),
+  addClass = document.querySelector(".site"),
+  overlay = document.querySelector(".overlay");
+
+menuButton.addEventListener("click", function () {
+  addClass.classList.toggle("showmenu");
+});
+closeButton.addEventListener("click", function () {
+  addClass.classList.remove("showmenu");
+});
+overlay.addEventListener("click", function () {
+  addClass.classList.remove("showmenu");
+});
+
+// show sub menu on mobile
+const submenu = document.querySelectorAll(".has-child .icon-small");
+submenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  submenu.forEach((item) =>
+    item != this ? item.closest(".has-child").classList.remove("expand") : null
+  );
+  if (this.closest(".has-child").classList != "expand");
+  this.closest(".has-child").classList.toggle("expand");
+}
+
+// slider
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// show search on mobile
+const searchButton = document.querySelector(".t-search"),
+  tClose = document.querySelector(".search-close"),
+  showClass = document.querySelector(".site");
+searchButton.addEventListener("click", function () {
+  showClass.classList.toggle("showsearch");
+});
+tClose.addEventListener("click", function () {
+  showClass.classList.remove("showsearch");
+});
